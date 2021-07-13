c69aa89a720b31c83060fb6872cac37b0d9aae97
breannams <breannamarie_s@hotmail.com>
2021-07-12 21:34:49 -0400

    commit tree 0fd9f061dcd418b6770ea2ea6e855e5b23c972a4

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-'\. -]+/g, '');
+  }
+
+
+  static titleize(string) {
+    const forbidden = ['a','an','the','but','of','and','for','at','by','from']
+
+    let newString = []
+
+    string.split(" ").forEach(word => {
+      if (forbidden.includes(word) === false) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      else {
+        word
+      }
+      newString.push(word)
+    })
+    let joinString = newString.join(' ')
+    return joinString.charAt(0).toUpperCase() + joinString.slice(1)
+  }
 }
